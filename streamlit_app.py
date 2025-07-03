9e1785269fc0c4f270e9b253d817ed9b7c893cce
Amila803 <amilailyana112@gmail.com>
2025-07-03 22:48:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -380,7 +380,7 @@ def main():
                 st.info("This is an international trip")
 
         with st.form("travel_form"):
-        col1, col2 = st.columns(2)
+            col1, col2 = st.columns(2)
 
         with col1:
             nationality = st.selectbox('Your Nationality', NATIONALITIES, index=0)
