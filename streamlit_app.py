27e54cdba52dfa039c007229cc23567b4351126b
Amila803 <amilailyana112@gmail.com>
2025-07-04 15:09:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,7 +320,7 @@ if data is not None:
     
     # Train model
     if st.button("Train Model"):
-        with st.st.spinner("Training model..."):
+        with st.spinner("Training model..."):
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             
             # Define accommodation type weights (resort most expensive, hostel cheapest)
